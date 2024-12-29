f6ca826df86e1403780745835728c7aacb35f4bb
Akshat Gupta <akshatgupta9612@gmail.com>
2024-12-29 13:37:11 +0530

    fixed all register controller bugs

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,23 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const tempDir = path.resolve("./public/temp");
+
+// Ensure the directory exists
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        cb(null, tempDir);
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+        cb(null, file.originalname);
+    },
+});
+
+export const upload = multer({
+    storage,
+});
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import {v8 as cloudinary} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({ 
@@ -11,15 +11,17 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
+        console.log(localFilePath)
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
         // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
+        console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
+        console.log("here")
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
